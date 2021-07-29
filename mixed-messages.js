a8907ddf681a3f3ca01e90c261d1f0ec23919f59
hypercyte <hypercyte@gmail.com>
2021-07-29 17:33:12 +0100

    output and cleanup

--- mixed-messages.js
+++ mixed-messages.js
@@ -4,31 +4,33 @@ Mujahid Ahmed July 2021
 */
 
 /**
- * Word bank
+ * Word bank.
  */
 
-const commonWords = ["a", "the", "that"];
-
-const nouns = ["boy", "dude", "girl", "plane", "mouse", "man", "woman", "sound", "telephone", "scientist", "glass", "note", "word", "fan", "disk", "knife"];
-
-const verbs = ["slapped", "poked", "helped", "removed", "obliterated", "kissed", "hugged", "walked to", "stopped", "injured", "embraced", "ignored", "chose to ignore", "didn't see", "walked over", "switched on"];
-
-/**
- * Generate random numbers
- */
-
-const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-const commonIndex1 = randomIntFromInterval(0, commonWords.length);
-
-const nounIndex1 = randomIntFromInterval(0, nouns.length);
-
-const verbIndex = randomIntFromInterval(0, verbs.length);
-
-const commonIndex2 = randomIntFromInterval(0, commonWords.length);
-
-const nounIndex2 = randomIntFromInterval(0, nouns.length);
-
-
+ const commonWords   = ["a", "the", "that"];
+ const nouns         = ["boy", "dude", "girl", "plane", "mouse", "man", "woman", "sound", "telephone", "scientist", "glass", "note", "word", "fan", "disk", "knife"];
+ const verbs         = ["slapped", "poked", "helped", "removed", "obliterated", "kissed", "hugged", "walked to", "stopped", "injured", "embraced", "ignored", "chose to ignore", "didn't see", "walked over", "switched on"];
+ 
+ /**
+  * Generate random numbers.
+  */
+ 
+ const randomIntFromInterval = (min, max) => {
+     return Math.floor(Math.random() * (max - min + 1) + min);
+ }
+ 
+ const commonIndex1  = randomIntFromInterval(0, commonWords.length);
+ const nounIndex1    = randomIntFromInterval(0, nouns.length);
+ const verbIndex     = randomIntFromInterval(0, verbs.length);
+ const commonIndex2  = randomIntFromInterval(0, commonWords.length);
+ const nounIndex2    = randomIntFromInterval(0, nouns.length);
+ 
+ /**
+  * Output random sentence.
+  */
+ 
+ console.log(commonWords[commonIndex1]   + ' ' +
+             nouns[nounIndex1]           + ' ' +
+             verbs[verbIndex]            + ' ' +
+             commonWords[commonIndex2]   + ' ' +
+             nouns[nounIndex2]           + '.' + '\n');
